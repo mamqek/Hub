a46597a9cec3913361981ca2356e9540accb421b
mamqek <65137206+mamqek@users.noreply.github.com>
2024-07-30 22:01:22 +0200

    homepage added

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="container-center">
+    <h1>Home</h1>
+
+    <p>Welcome to the home page!</p>
+
+    <p>Here you can find information about our company and our products.</p>
+
+    <p>Feel free to browse around and contact us if you have any questions.</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path: "/",
-        component: () => import("./App.vue"),
+        component: () => import("./pages/Home.vue"),
     }, 
     {
         path: "/zip-code-checker",
