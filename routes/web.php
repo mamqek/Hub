d8f8258fdea58c4ceb57315722fef65a26753bcd
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-03 17:04:34 +0200

    added get link

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/check-zip-code', 'App\Http\Controllers\ZipCodeCheckerController@checkZipCode');
+Route::post('/check-zip-code', 'App\Http\Controllers\BagAPIController@checkZipCode');
+
+Route::get('/check-zip-code', 'App\Http\Controllers\BagAPIController@link');
 
 
 Route::get('/{vue_capture?}', function () {
