4f78313a3f0a7383cd51ad083cd104d7f88b7f30
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-03 17:01:56 +0200

    show buttons for links provided in resonse to get more info component. Doent show when links were already fethced

---
+++ resources/js/components/LinksButtons.vue
@@ -0,0 +1,28 @@
+<template>
+    <template v-for="(link, linkname) in links" :key="linkname">
+        <template v-if="!alreadyFetched.has(linkname) && linkname !='self'">
+            <button v-if="Array.isArray(link)" 
+                class="btn"
+                v-for="(singleUrlObj, index) in link" :key="index" 
+                @click="$emit('send-link', linkname, singleUrlObj.href)"
+            >
+                Retrieve {{ linkname }}
+            </button>
+            <button v-else 
+                class="btn"
+                @click="$emit('send-link', linkname, link.href)"
+            >
+                Retrieve {{ linkname }}
+            </button>
+        </template>
+    </template>
+</template>
+
+<script>
+    export default {
+        name: "Links Buttons",
+
+        props: ["links", "alreadyFetched"],
+        emits: ['send-link'],
+    }
+</script>
