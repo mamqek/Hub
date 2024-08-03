ccb206cf20505485575dda11c8069fb48fdc0b73
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-03 17:00:31 +0200

    migrations for saving requests responses in db

---
+++ database/migrations/2024_08_03_094755_create_address_searches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address_searches', function (Blueprint $table) {
+            $table->id();
+            $table->string('postcode');
+            $table->string('huisnummer');
+            $table->string('huisletter')->nullable();
+            $table->integer('search_count')->default(0);
+            $table->integer('exacteMatch')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('search_address');
+    }
+};
---
+++ database/migrations/2024_08_03_101501_create_address_responses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address_responses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('address_id');
+            $table->string('endpoint');
+            $table->json('response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address_responses');
+    }
+};
