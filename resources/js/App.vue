37d128419f45d4fd03a455476598b2b3e7240411
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-03 16:59:38 +0200

    layout styles changed

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,11 @@
 <template>    
     <nav>
-        <router-link to="/">Home</router-link>
         <router-link to="/zip-code-checker">Zip Code Checker</router-link>
+        <router-link to="/">Home</router-link>
     </nav>
 
     <router-view v-slot="{ Component, route }">
-        <div :key="route.name" id="content-wrapper">
+        <div :key="route.name" id="layout">
             <Component :is="Component" />
         </div>
     </router-view>
