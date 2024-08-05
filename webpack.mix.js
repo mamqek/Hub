dc580b6dd65154c7f13c6fdbfecb72912f4bdb17
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-05 12:51:51 +0200

    eslint doesnt work and only creates problems, try later, now delete

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,8 @@
 let mix = require('laravel-mix');
-require('laravel-mix-eslint');
-
 
 
 mix.js('resources/js/app.js', 'public/js')
     .vue({ version: 3 })
-    .eslint({
-        fix: true, // Automatically fix linting errors
-        extensions: ['js', 'vue'],
-      });
    
 if (mix.inProduction()) {
     mix.version();
