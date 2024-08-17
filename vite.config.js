6ee6f804871a4ed84be323ab6ffe5c2a74bce0aa
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-17 08:41:12 +0200

    add @ for base path reference

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
+
 
 
 export default defineConfig({
@@ -14,6 +16,7 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: "vue/dist/vue.esm-bundler.js",
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url))
         },
     },
 });
