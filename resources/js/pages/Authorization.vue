3b6c1ed8689a5ea6cfd5dd10a737a97e238fcc72
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-17 08:39:05 +0200

    Authorization page with login form

---
+++ resources/js/pages/Authorization.vue
@@ -0,0 +1,57 @@
+<template>
+    <form @submit.prevent="login"class="content">
+
+        <InputText v-model="username"
+            label="Username"
+            id="username"
+            required
+        />
+        <InputText v-model="password"
+            label="Password"
+            id="password"
+            type="password"
+            required
+        />
+        
+        <button class="btn">Login</button>
+    </form>
+</template>
+    
+<script>
+import InputText from "@/elements/InputText.vue"
+
+export default {
+    name: 'Authorization',
+
+    data(){
+        return {
+            username: "test@example.com",
+            password: "password",
+        }
+    },
+
+    components: {
+        InputText
+    },
+
+    methods: {
+        login() {
+            this.$axios.post("/login", {
+                username: this.username,
+                password: this.password
+            })
+            .then(({data}) => {
+                console.log(data)
+            })
+            .catch(error => {
+                alert(error.message)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
