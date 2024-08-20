e8c2410ed0f4a0b5b6be5681d699bcc54ad07037
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-20 20:12:48 +0200

    authorization page style fully reworked

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        <!-- Icons -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
         <div id="app"></div>
