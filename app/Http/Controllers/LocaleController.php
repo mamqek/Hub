930817d4b22b3c61352a4368b73b357b5f9b3ab7
mamqek <65137206+mamqek@users.noreply.github.com>
2024-08-26 11:30:16 +0200

    backend change locale and save in session

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
-
 
 class LocaleController extends Controller
 {
 
     function getMessages($locale) {
         $path = base_path("lang/{$locale}/main.json");
+
         if (!File::exists($path)) {
             abort(404, 'Translation file not found.');
         }
@@ -19,13 +21,33 @@ class LocaleController extends Controller
         return response()->file($path);
     }
 
-    function getLanguages() {
+    function getLocales() {
         $langDirectory = base_path("lang");
         $subDirectories = File::directories($langDirectory);
-        $languages = array_map(function($dir) {
+        $locales = array_map(function($dir) {
             return basename($dir);
         }, $subDirectories);
 
-        return $languages;
+        return $locales;
+    }
+
+    function changeLocale($locale) {
+        try {
+            App::setLocale($locale);
+            session(['locale' => $locale]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Succesfully changed locale',
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while changing the locale',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('translations/{locale}', ['App\Http\Controllers\LocaleController', 'getMessages']);
-Route::get('translations', ['App\Http\Controllers\LocaleController', 'getLanguages']);
+Route::get('translations', ['App\Http\Controllers\LocaleController', 'getLocales']);
+Route::post('translations/{locale}', ['App\Http\Controllers\LocaleController', 'changeLocale']);
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', ['App\Http\Controllers\UserController', 'login']);
@@ -25,6 +23,7 @@ Route::group(['prefix' => 'soulmap'], function () {
     Route::get('/clients', 'App\Http\Controllers\SoulClientController@getAllClients');
     Route::post('/saveClient', 'App\Http\Controllers\SoulClientController@saveClient');
 });
+
 
 
 Route::get('/{vue_capture?}', function () {
