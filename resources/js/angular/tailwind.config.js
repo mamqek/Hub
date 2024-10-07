cf800779b72a60ab4d88a4d57835bb9bd165ba8a
mamqek <65137206+mamqek@users.noreply.github.com>
2024-10-07 15:12:10 +0200

    installed tailwind

---
+++ resources/js/angular/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}", // Update this path according to your file structure
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
