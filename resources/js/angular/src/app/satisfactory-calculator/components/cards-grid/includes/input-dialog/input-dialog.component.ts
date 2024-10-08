2ba8d126459d6af7794bf609cf8a0e4ff0788206
mamqek <65137206+mamqek@users.noreply.github.com>
2024-10-08 12:27:09 +0200

    dialog to enter recipe with text input done

--- resources/js/angular/src/app/satisfactory-calculator/components/cards-grid/includes/input-dialog/input-dialog.component.ts
+++ resources/js/angular/src/app/satisfactory-calculator/components/cards-grid/includes/input-dialog/input-dialog.component.ts
@@ -1,15 +1,38 @@
 import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { InputNumberComponent } from 'app/input-number/input-number.component';
+
 
 @Component({
-  selector: 'input-dialog',
-  standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
-  templateUrl: './input-dialog.component.html',
-  styleUrl: './input-dialog.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'input-dialog',
+    standalone: true,
+    imports: [
+        MatDialogModule, 
+        MatButtonModule, 
+        MatInputModule, 
+        MatFormFieldModule, 
+        InputNumberComponent,
+        FormsModule
+    ],
+    templateUrl: './input-dialog.component.html',
+    styleUrl: './input-dialog.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputDialogComponent {
+    item: string = '';
+    amount: number = 0;
 
+    constructor(private dialogRef: MatDialogRef<InputDialogComponent>) {}
+
+    submit() {
+        // Close the dialog and pass the data back        
+        this.dialogRef.close({
+            item: this.item, 
+            amount: this.amount 
+        });
+    }
 }
