3b32a68197fd4276951a1b5e1fec07bdcfb6154d
mamqek <65137206+mamqek@users.noreply.github.com>
2024-10-11 04:35:43 +0200

    circles with output items added to card

--- resources/js/angular/src/app/satisfactory-calculator/components/satisfactory-card/satisfactory-card.component.ts
+++ resources/js/angular/src/app/satisfactory-calculator/components/satisfactory-card/satisfactory-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterContentInit, OnDestroy } from '@angular/core';
 import { RecipeNode } from '../../services/recipe.service';
 
 import { IngredientsService } from 'app/satisfactory-calculator/services/ingredients.service';
